2a471a560a8ea52b2187261414b2cfa5559e44ca
abrahamayquipalatam <abraham.ayquipa@latam.com>
2026-03-22 12:03:50 -0500

    ci: correct the prisma configurations

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const prisma = new PrismaClient()
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 export { prisma }
